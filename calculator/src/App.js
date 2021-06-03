a21a82690f4617fe6bc7ee848a5ca9ea8c52752f
kapil <kapsstudy@gmail.com>
2021-06-03 21:21:54 +0530

    adding all functionality

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const clickHandler = (e) => {
+    setResult(result.concat(e.target.name)); // doing Concatenation 
+  }
+
+  const clearHandler = () => {
+    setResult("");
+  }
+  const backspace = () => {
+    setResult(result.slice(0, result.length - 1));
+  }
+
+  const totalHandler = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" value={result} />
+      </div>
+      <div className="keypad">
+        <button onClick={clearHandler}>clear</button>
+        <button onClick={backspace}>C</button>
+        <button name="/" onClick={clickHandler}>&divide;</button>
+        <button name="1" onClick={clickHandler}>1</button>
+        <button name="2" onClick={clickHandler}>2</button>
+        <button name="3" onClick={clickHandler}>3</button>
+        <button name="*" onClick={clickHandler}>&times;</button>
+        <button name="4" onClick={clickHandler}>4</button>
+        <button name="5" onClick={clickHandler}>5</button>
+        <button name="6" onClick={clickHandler}>6</button>
+        <button name="+" onClick={clickHandler}>+</button>
+        <button name="7" onClick={clickHandler}>7</button>
+        <button name="8" onClick={clickHandler}>8</button>
+        <button name="9" onClick={clickHandler}>9</button>
+        <button name="-" onClick={clickHandler}>&ndash;</button>
+        <button name="0" onClick={clickHandler}>0</button>
+        <button name="." onClick={clickHandler}>.</button>
+        <button onClick={totalHandler}>=</button>
+      </div>
     </div>
   );
 }
